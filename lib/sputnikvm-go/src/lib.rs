b816f55235d5bdf3a4e40dbe6489416ea0cffb0a
Wei Tang <hi@that.world>
2017-11-23 16:31:14 +0800

    Convertion from c types to actual sputnikvm types

--- lib/sputnikvm-go/src/lib.rs
+++ lib/sputnikvm-go/src/lib.rs
@@ -1,16 +1,15 @@
 extern crate libc;
+extern crate bigint;
+extern crate sputnikvm;
 
+mod common;
+
+pub use common::{c_address, c_gas, c_u256};
+
+use std::slice;
+use std::rc::Rc;
 use libc::{c_uchar, c_uint, c_longlong};
-
-pub struct c_address {
-    pub data: [c_uchar; 20],
-}
-pub struct c_gas {
-    pub data: [c_uchar; 32],
-}
-pub struct c_u256 {
-    pub data: [c_uchar; 32],
-}
+use sputnikvm::{TransactionAction, ValidTransaction, HeaderParams, SeqTransactionVM, Patch};
 
 type c_action = c_uchar;
 pub const CALL_ACTION: c_action = 0;
@@ -36,4 +35,36 @@ pub struct c_header_params {
     pub number: c_u256,
     pub difficulty: c_u256,
     pub gas_limit: c_gas,
+}
+
+#[no_mangle]
+fn sputnikvm_new<P: Patch>(
+    transaction: c_transaction, header: c_header_params
+) -> *mut SeqTransactionVM<P> {
+    let transaction = ValidTransaction {
+        caller: Some(transaction.caller.into()),
+        gas_price: transaction.gas_price.into(),
+        gas_limit: transaction.gas_limit.into(),
+        action: if transaction.action == CALL_ACTION {
+            TransactionAction::Call(transaction.action_address.into())
+        } else if transaction.action == CREATE_ACTION {
+            TransactionAction::Create
+        } else {
+            panic!()
+        },
+        value: transaction.value.into(),
+        input: {
+            let s = unsafe {
+                slice::from_raw_parts(transaction.input, transaction.input_len as usize)
+            };
+            let mut r = Vec::new();
+            for v in s {
+                r.push(*v);
+            }
+            Rc::new(r)
+        },
+        nonce: transaction.nonce.into(),
+    };
+
+    unimplemented!()
 }
