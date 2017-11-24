786bbcb3a9c2635952b7b774488f150d6ac87687
Wei Tang <hi@that.world>
2017-11-24 23:47:28 +0800

    Add examples for sputnikvm_fire

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ package main
 */
 import "C"
 
+import (
+	"fmt"
+)
+
 func main() {
 	transaction := C.sputnikvm_default_transaction()
 	header := C.sputnikvm_default_header_params()
 	vm := C.sputnikvm_new_frontier(transaction, header)
+	ret := C.sputnikvm_fire(vm)
+	fmt.Printf("%v", ret)
 	C.sputnikvm_free(vm)
 }
